133e712ee921c940e365e86da1b7ecbbdb7af8d9
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2025-12-07 15:04:52 +0500

    init

---
+++ src/test/java/uzumtech/bill_splitter/BillSplitterApplicationTests.java
@@ -0,0 +1,13 @@
+package uzumtech.bill_splitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillSplitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
